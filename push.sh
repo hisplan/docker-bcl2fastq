3abeda76f9172082c73fe27923f359945eb19d3f
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2020-09-02 11:54:02 -0400

    Add version info

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker login
-docker tag bcl2fastq hisplan/bcl2fastq
-docker push hisplan/bcl2fastq
+docker tag bcl2fastq hisplan/bcl2fastq:2.20.0
+docker push hisplan/bcl2fastq:2.20.0
